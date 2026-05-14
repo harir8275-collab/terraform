b23e8de1c7c2c1270d9ad85cf6226350e5fc811f
Hari Rahul <harir8275@gmail.com>
2026-05-14 10:45:23 +0530

    day-4

---
+++ day-4-IAM-role/main.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "app_role" {
+  name = "app-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "custom_policy" {
+  name = "custom-s3-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = aws_iam_policy.custom_policy.arn
+}
